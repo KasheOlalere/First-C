a356d2918d037d4a47f7c4e017bbd7ce115186c8
Abdullah <49123747+Kashe13@users.noreply.github.com>
2019-03-31 20:52:53 +0400

    First try

---
+++ dodo.py
@@ -0,0 +1,28 @@
+print ('Welcome')
+print ('What is your name ')
+name = input()
+print ('Hello '+ name)
+import random
+number = random.randint(1,20)
+guessTaken = 0
+while guessTaken < 6:
+        print ('Take a Guess:')
+        guess = input()
+        guess = int(guess)
+        guessTaken = guessTaken + 1
+        if guess > number + 5:
+                print ('Your guess is too high')
+        elif guess > number:
+                print ('You are close,Try again')
+        elif guess < number -5:
+                print ('Your guess is too low')
+        elif guess < number:
+                print('You are close, Try again')
+        elif guess == number:
+                break
+if guess== number:
+        guessTaken = str(guessTaken)
+        print ('You guessed my number in ' +guessTaken + ' tries')
+if guess!= number:
+        print ('nice try, but the number i was thinking of was '+ number)
+            
